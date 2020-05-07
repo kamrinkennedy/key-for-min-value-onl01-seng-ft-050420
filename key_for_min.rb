1199a4872d0e015a81bb135b57d34eaae3f60fd5
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-07 16:46:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_key_value = 0
-  desired_key = ""
+  desired_key = nil
   name_hash.each do |key, value|
     if value < smallest_key_value
       smallest_key_value = value
